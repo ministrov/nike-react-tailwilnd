645c44a890e884f0eca9edd9e073e708283589e6
unknown <ministrov2018@gmail.com>
2024-03-22 13:13:37 +0300

    keep on operating on new burger menu realization approach

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,48 +1,61 @@
 import { useState } from "react";
 import { navLinks } from "../constants";
 import MenuSvg from "./design/MenuSvg";
+import { HamburgerMenu } from "./design/HamburgerMenu";
 import headerLogo from "../assets/images/header-logo.svg";
-// import { hamburger } from "../assets/icons";
 
 const Nav = () => {
-  const [open, setOpen] = useState(false);
+  const [openNavigation, setOpenNavigation] = useState(false);
 
-  console.log(open);
+  console.log(openNavigation);
 
-  const handleToggleMenu = () => {
-    setOpen((open) => !open);
-  }
+  const toggleNavigation = () => {
+    if (openNavigation) {
+      setOpenNavigation(false);
+    } else {
+      setOpenNavigation(true);
+    }
+  };
 
   return (
     <header className="padding-x py-8 absolute z-10 w-full">
-      <nav className="flex justify-between items-center max-container">
+      <div className="flex justify-between max-container">
         <a href="/">
-          <img 
-            src={headerLogo} 
+          <img
+            src={headerLogo}
             className="ring-purple-500"
-            width={130} 
-            height={29} 
-            alt="Logo" 
+            width={130}
+            height={29}
+            alt="Logo"
           />
         </a>
+        <nav className={`${
+            openNavigation ? "flex" : "hidden"
+          } fixed top-20 left-0 right-0 bottom-0 bg-white lg:static lg:flex lg:mx-auto lg:bg-transparent`}>
+          <ul className="relative z-50 w-full flex flex-col gap-9 items-center justify-center lg:flex-row">
+            {navLinks.map((item) => (
+              <li key={item.label}>
+                <a
+                  href={item.href}
+                  className="font-montserrat leading-normal hover:text-purple-500 text-lg text-slate-gray"
+                >
+                  {item.label}
+                </a>
+              </li>
+            ))}
+          </ul>
 
-        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
-          {navLinks.map((item) => (
-            <li key={item.label}>
-              <a 
-                href={item.href}
-                className="font-montserrat leading-normal hover:text-purple-500 text-lg text-slate-gray"
-              >
-                {item.label}
-              </a>
-            </li>
-          ))}
-        </ul>
+          <HamburgerMenu />
+        </nav>
 
-        <div role="button" onClick={handleToggleMenu} className="hidden max-lg:block">
-          <MenuSvg/>
+        <div
+          role="button"
+          onClick={toggleNavigation}
+          className="ml-auto hidden max-lg:block border-2 border-purple-500 rounded p-2"
+        >
+          <MenuSvg openNavigation={openNavigation}/>
         </div>
-      </nav>
+      </div>
     </header>
   );
 };
