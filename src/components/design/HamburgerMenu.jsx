3b0cc37edf3c53587c6102380f8f7213a8d77199
unknown <ministrov2018@gmail.com>
2024-03-25 13:15:15 +0300

    fix: check all browser compatibility

--- src/components/design/HamburgerMenu.jsx
+++ src/components/design/HamburgerMenu.jsx
@@ -1,8 +1,8 @@
 export const SideLines = () => {
   return (
     <>
-      <div className="absolute top-0 left-5 w-0.25 h-full bg-n-6"></div>
-      <div className="absolute top-0 right-5 w-0.25 h-full bg-n-6"></div>
+      <div className="absolute top-0 left-5 w-0.5 h-full bg-slate-200"></div>
+      <div className="absolute top-0 right-5 w-0.5 h-full bg-slate-200"></div>
     </>
   );
 };
